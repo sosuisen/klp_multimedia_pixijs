ffa60d5c85fb2f940eb3d51d94d9c10e9a374870
sosuisen <hidekazu.kubota@gmail.com>
2023-02-24 01:52:43 +0900

    feat: add pixi-gsap

--- pixi-gsap.js
+++ pixi-gsap.js
@@ -1,38 +1,59 @@
-let app = new PIXI.Application({ width: 640, height: 360 });
+let app = new PIXI.Application({ width: 640, height: 640 });
+app.ticker.stop();
 document.body.appendChild(app.view);
 
-// kyoco.png(240x280)
-let sprite = PIXI.Sprite.from('kyoco.png');
-sprite.y = 140;
-sprite.anchor.x = 0.5;
-sprite.anchor.y = 0.5;
-
-app.stage.addChild(sprite);
-
-app.ticker.stop();
+const sprites = [];
+const colors = [ 0xffffff, 0xffff70, 0xff7070, 0x70ff70 ];
+for (let i = 0; i < colors.length; i++) {
+  // kyoco.png(240x280)
+  let sprite = PIXI.Sprite.from('kyoco.png');
+  sprite.x = 100;
+  sprite.y = 80 + i * 150;
+  sprite.scale.x = 0.5;
+  sprite.scale.y = 0.5;
+  sprite.anchor.x = 0.5;
+  sprite.anchor.y = 0.5;
+  sprite.tint = colors[i];
+  app.stage.addChild(sprite);
+  sprites.push(sprite);
+}
 
 // GSAPのtickを使う
-let elapsed = 0.0;
-gsap.ticker.add(() => {
+gsap.ticker.add(time => {
   app.ticker.update(); // PixiJSの画面更新
-  // elapsed += delta;
-  // sprite.x = 320.0 + Math.cos(elapsed / 50.0) * 100.0;
+  // 引き続きtimeを使った処理もできる。
+  // GSAPのtimeはtickerが開始してからの時間
+  sprites[0].y = 80 + 10 * Math.cos(time*5); // 上下に揺れ
 });
 
-gsap.to(sprite, {
-  duration: 2,
-  yoyo: true,
-  repeat: -1,
-  pixi: {
-    scaleX: 1.5,
-    scaleY: 1.5,
-  }
+// GSAPのTween が使える
+/*
+gsap.to(sprites[0], 1, { 
+  x: 400,
 });
+*/
 
-gsap.to(sprite, {
-  duration: 2,
-  repeat: -1,
+// Stagger
+// scaleやangleのようなサブオブジェクトはpixiプロパティ経由でアクセス
+/*
+gsap.to(sprites, 1, { 
+  x:200,
+  stagger: 0.5,
   pixi: {
-    rotation: 360,
-  }
+    angle: 360
+  },
 });
+*/
+
+// Timeline
+/*
+let tl = gsap.timeline()
+// let tl = gsap.timeline({ repeat: -1, repeatDelay: 1, yoyo: true })
+tl.to(sprites[0], { x: 480, duration: 1.5, ease: 'power3.out' });
+tl.to(sprites[1], { x: 480, duration: 0.5, delay:0.2, ease: 'power3.out' });
+tl.to(sprites[2], { x: 480, duration: 0.5, ease: 'power3.out' });
+*/
+
+// tickerで動かしているアニメは停止しない
+document.getElementById('pauseBtn').addEventListener('click', () => tl.pause());
+document.getElementById('resumeBtn').addEventListener('click', () => tl.resume());
