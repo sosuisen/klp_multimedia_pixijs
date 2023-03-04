05ac1de2f7f90dd20b2c3d4ccb612f8dc8e5afa6
sosuisen <hidekazu.kubota@gmail.com>
2023-03-05 00:39:47 +0900

    fix: use gsap v3

--- pixi-gsap.js
+++ pixi-gsap.js
@@ -1,9 +1,11 @@
 import * as PIXI from 'pixi.js';
 import { gsap } from "gsap";
-import { PixiPlugin } from "gsap/PixiPlugin.js";
+import { PixiPlugin } from "gsap/PixiPlugin";
+
+gsap.registerPlugin(PixiPlugin); // プラグインを登録
+PixiPlugin.registerPIXI(PIXI); // プラグインへPIXIオブジェクトへの参照を渡す
 
 let app = new PIXI.Application({ width: 640, height: 640 });
-gsap.registerPlugin(PixiPlugin);
 
 app.ticker.stop();
 document.body.appendChild(app.view);
@@ -11,8 +13,7 @@ document.body.appendChild(app.view);
 const sprites = [];
 const colors = [ 0xffffff, 0xffff70, 0xff7070, 0x70ff70 ];
 for (let i = 0; i < colors.length; i++) {
-  // kyoco.png(240x280)
-  let sprite = PIXI.Sprite.from('kyoco.png');
+  let sprite = PIXI.Sprite.from('kyoco_trans256x256.png');
   sprite.x = 100;
   sprite.y = 80 + i * 150;
   sprite.scale.x = 0.5;
@@ -34,34 +35,34 @@ gsap.ticker.add(time => {
 
 // GSAPのTween が使える
 /*
-gsap.to(sprites[0], 1, { 
+gsap.to(sprites[0], { 
+  duration: 1,
   x: 400,
 });
 */
 
 /**
  * Stagger（重ならないように調整する、の意味）
- */ 
-
-// scaleやangleのようなサブオブジェクトはpixiプロパティ経由でアクセス
+ */
 /*
-gsap.to(sprites, 1, { 
-  x:200,
+gsap.to(sprites, { 
+  duration: 1,
+  x: 200,
   stagger: 0.5,
   pixi: {
-    angle: 360
+    rotation: 360,
   },
 });
 */
 
 // Timeline
-
 const tl = gsap.timeline()
-// const tl = gsap.timeline({ repeat: -1, repeatDelay: 1, yoyo: true })
+// let tl = gsap.timeline({ repeat: -1, repeatDelay: 1, yoyo: true })
 tl.to(sprites[0], { x: 480, duration: 1.5, ease: 'power3.out' });
 tl.to(sprites[1], { x: 480, duration: 0.5, delay:0.2, ease: 'power3.out' });
 tl.to(sprites[2], { x: 480, duration: 0.5, ease: 'power3.out' });
 tl.to(sprites[3], { x: 360, duration: 1.0, ease: 'bounce.out' });
+
 
 // tickerで動かしているアニメはこのpause()では停止しない
 document.getElementById('pauseBtn').addEventListener('click', () => tl.pause());
