a6fa6faa8978057f6f5c00d1c64852ed76cdfbb9
sosuisen <hidekazu.kubota@gmail.com>
2023-03-05 00:00:32 +0900

    feat: add completed version of practice

--- pixi.js
+++ pixi.js
@@ -1,3 +1,14 @@
 import * as PIXI from 'pixi.js'
 
 let app = new PIXI.Application({ width: 640, height: 360 });
+
+document.body.appendChild(app.view);
+
+let sprite = PIXI.Sprite.from('kyoco_trans256x256.png');
+app.stage.addChild(sprite);
+
+let elapsed = 0.0;
+app.ticker.add((delta) => {
+  elapsed += delta;
+  sprite.x = 100.0 + Math.cos(elapsed/50.0) * 100.0;
+});
