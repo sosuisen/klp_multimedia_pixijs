41e0882f73fdf4c259788b6f2f6d9e6ebd5e7542
sosuisen <hidekazu.kubota@gmail.com>
2023-03-05 00:46:19 +0900

    feat: add answer

--- pixi-gsap.js
+++ pixi-gsap.js
@@ -1,9 +1,11 @@
 import * as PIXI from 'pixi.js';
 import { gsap } from "gsap";
-import { PixiPlugin } from "gsap/PixiPlugin.js";
+import { PixiPlugin } from "gsap/PixiPlugin";
+
+gsap.registerPlugin(PixiPlugin); // プラグインを登録
+PixiPlugin.registerPIXI(PIXI); // プラグインへPIXIオブジェクトへの参照を渡す
 
 let app = new PIXI.Application({ width: 640, height: 640 });
-gsap.registerPlugin(PixiPlugin);
 
 app.ticker.stop();
 document.body.appendChild(app.view);
@@ -11,8 +13,7 @@ document.body.appendChild(app.view);
 const sprites = [];
 const colors = [ 0xffffff, 0xffff70, 0xff7070, 0x70ff70 ];
 for (let i = 0; i < colors.length; i++) {
-  // kyoco.png(240x280)
-  let sprite = PIXI.Sprite.from('kyoco.png');
+  let sprite = PIXI.Sprite.from('kyoco_trans256x256.png');
   sprite.x = 100;
   sprite.y = 80 + i * 150;
   sprite.scale.x = 0.5;
@@ -26,41 +27,11 @@ for (let i = 0; i < colors.length; i++) {
 
 // GSAPのtickを使う
 gsap.ticker.add(time => {
-  app.ticker.update(); // PixiJSの画面更新
-  // 引き続きtimeを使った処理もできる。
-  // GSAPのtimeはtickerが開始してからの時間
-  // sprites[0].y = 80 + 10 * Math.cos(time*5); // 上下に揺れ
-  sprites[0].alpha = Math.abs(Math.cos(time));
+  app.ticker.update();
+  sprites[0].alpha = Math.abs(Math.sin(time));
+  sprites[1].alpha = Math.abs(Math.cos(time));
 });
 
-// GSAPのTween が使える
-/*
-gsap.to(sprites[0], 1, { 
-  x: 400,
-});
-*/
-
-// Stagger
-// scaleやangleのようなサブオブジェクトはpixiプロパティ経由でアクセス
-/*
-gsap.to(sprites, 1, { 
-  x:200,
-  stagger: 0.5,
-  pixi: {
-    angle: 360
-  },
-});
-*/
-
-// Timeline
-/*
-let tl = gsap.timeline()
-// let tl = gsap.timeline({ repeat: -1, repeatDelay: 1, yoyo: true })
-tl.to(sprites[0], { x: 480, duration: 1.5, ease: 'power3.out' });
-tl.to(sprites[1], { x: 480, duration: 0.5, delay:0.2, ease: 'power3.out' });
-tl.to(sprites[2], { x: 480, duration: 0.5, ease: 'power3.out' });
-*/
-
-// tickerで動かしているアニメは停止しない
+// tickerで動かしているアニメはこのpause()では停止しない
 document.getElementById('pauseBtn').addEventListener('click', () => tl.pause());
 document.getElementById('resumeBtn').addEventListener('click', () => tl.resume());
