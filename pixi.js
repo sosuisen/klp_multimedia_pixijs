666e870c049ffc09742846e9e9199923caad47e8
sosuisen <hidekazu.kubota@gmail.com>
2023-05-16 15:37:44 +0900

    feat: add answer

--- pixi.js
+++ pixi.js
@@ -1,3 +1,21 @@
-import * as PIXI from 'pixi.js'
+import * as PIXI from "pixi.js";
 
-let app = new PIXI.Application({ width: 640, height: 360 });
+const app = new PIXI.Application({ width: 640, height: 360 });
+
+// app.viewはPixiJSの描画先となるcanvas要素
+document.body.appendChild(app.view);
+
+const sprite = PIXI.Sprite.from("kyoco_trans256x256.png");
+const sprite2 = PIXI.Sprite.from("kyoco_trans256x256.png");
+
+app.stage.addChild(sprite);
+
+app.stage.addChild(sprite2);
+sprite2.x = 300;
+
+let elapsed = 0.0;
+app.ticker.add((delta) => {
+  elapsed += delta;
+  sprite.alpha = Math.abs(Math.sin(elapsed / 50.0));
+  sprite2.alpha = Math.abs(Math.cos(elapsed / 50.0));
+});
